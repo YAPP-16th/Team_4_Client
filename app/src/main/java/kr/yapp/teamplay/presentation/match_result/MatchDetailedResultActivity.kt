fa43d53274dc20fe94192872639b0f673456409b
LeeOhHyung <lee2928030@naver.com>
2020-06-14 17:49:44 +0900

    MatchDetailedResultActivity binding. LiveDataObserver, RecyclerView 설정 함수 초기 작업

--- app/src/main/java/kr/yapp/teamplay/presentation/match_result/MatchDetailedResultActivity.kt
+++ app/src/main/java/kr/yapp/teamplay/presentation/match_result/MatchDetailedResultActivity.kt
@@ -4,13 +4,39 @@
 package kr.yapp.teamplay.presentation.match_result
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import kr.yapp.teamplay.R
+
+import kr.yapp.teamplay.databinding.ActivityMatchDetailedResultBinding
 
 class MatchDetailedResultActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMatchDetailedResultBinding
+    private val viewModel: MatchDetailedResultViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setBinding()
+        setLiveDataObserver()
+        setRecyclerView()
 
+        // TODO ViewModel로 부터 경기 상세 결과를 가져오는 함수 호출
+    }
+
+    private fun setBinding() {
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_match_detailed_result)
+        binding.lifecycleOwner = this
+        binding.viewModel = viewModel
+    }
+
+    private fun setLiveDataObserver() {
+        // TODO ViewModel LiveData Observe
+    }
+
+    private fun setRecyclerView() {
+        // TODO 결기 상세 결과, 개인 기록 RecyclerView 초기화
     }
 
 }
