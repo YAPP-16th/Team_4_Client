a4f42eedbad66ce6b057413a63e63f2e90e99476
LeeOhHyung <lee2928030@naver.com>
2020-06-14 19:40:40 +0900

    경기 결과 matchId를 넘길때 기본값으로 1을 넘기도록 변경

--- app/src/main/java/kr/yapp/teamplay/presentation/teammain/PopupMatchResultActivity.kt
+++ app/src/main/java/kr/yapp/teamplay/presentation/teammain/PopupMatchResultActivity.kt
@@ -43,7 +43,7 @@ class PopupMatchResultActivity : AppCompatActivity() {
     private fun setListener() {
         binding.popupMatchResultGoButton.setOnClickListener {
             // TODO matchId 를 어디서 가져오지?
-            MatchDetailedResultActivity.start(this, matchId = -1)
+            MatchDetailedResultActivity.start(this, matchId = 1)
         }
     }
 }
